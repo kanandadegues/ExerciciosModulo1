23b94709908633b24904a96788d6a8f57ce07988
Kananda Degues <kananda.degues@gmail.com>
2023-02-27 12:47:23 -0300

    js funcional

--- pages/Semana 2/ex006/index.js
+++ pages/Semana 2/ex006/index.js
@@ -1,17 +1,18 @@
+
 let name = window.prompt('Seja Bem vindo(a)! Qual seu nome?');
-let student = window.prompt('Você é estudante do LAB 365? Digite sim ou não.');
-let gender = window.prompt(`Olá ${name}, qual seu gênero? Digite mulher ou homem.`);
-let age = Number(window.prompt('Insira sua idade'));
+let age = Number(window.prompt(`Olá ${name}, insira sua idade.`));
+let student = window.prompt("Você é estudante do LAB 365? Digite sim ou não.");
+let gender = window.prompt("Qual seu gênero? Digite mulher ou homem.");
 
 
-    if (student == "sim"){
+    if (student == "sim" && age >= 60){
+        window.alert("Você tem entrada gratuita!");
+    }else if (age >= 60){
         window.alert("Você tem entrada gratuita!");
     }else if (gender == "mulher" && age >=18){
         window.alert("Mulheres pagam 50,00 reais na entrada.");
     }else if (gender =! "mulher" && age >=18){
         window.alert("Homens pagam 80,00 reais na entrada.");
-    }else if (age >= 60){
-        window.alert("Você tem entrada gratuita!");
     }else {
         window.alert("Você não tem permissão para entrar.");
     }
