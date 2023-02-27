4e9ce07a94d42c37f7f74ba1cfd16ef765aa5d7e
Kananda Degues <kananda.degues@gmail.com>
2023-02-27 17:52:54 -0300

    update ex 007 semana 2

--- pages/Semana 2/ex007/index.js
+++ pages/Semana 2/ex007/index.js
@@ -7,8 +7,8 @@ let newSalary = salary;
 do{
     months++;
     if (months % 6 === 0) {
-        newSalary += newSalary * 0,1
+        newSalary += newSalary * 0.1
     }
 
-} while (salary < goal)
+} while (newSalary < goal)
 window.alert(`O seu salário será R$ ${goal} em ${months} meses.`)
